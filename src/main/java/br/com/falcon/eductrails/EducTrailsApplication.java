b0ffda6e73917f0070cf72aef90bdfc6cfa40ea1
Tyago Silva <tyagotayrony@gmail.com>
2019-08-31 10:59:52 -0300

    adição dos arquivos iniciais do projeto

---
+++ src/main/java/br/com/falcon/eductrails/EducTrailsApplication.java
@@ -0,0 +1,13 @@
+package br.com.falcon.eductrails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducTrailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducTrailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/falcon/eductrails/EducTrailsApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.falcon.eductrails;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EducTrailsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
